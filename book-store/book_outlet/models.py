c995258959bbb1964bca4a06f05efb9b1c4ab5ec
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2023-11-26 16:51:58 +0530

    urls rerouted using reverse

--- book-store/book_outlet/models.py
+++ book-store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 # Create your models here.
 
 class Book (models.Model):
@@ -11,3 +12,6 @@ class Book (models.Model):
 
     def __str__(self):
         return f"{self.title},{self.rating},{self.author},{self.is_bestselling}"
+    
+    def get_url(self):
+        return reverse("book_detail",args=[self.id])
--- book-store/book_outlet/views.py
+++ book-store/book_outlet/views.py
@@ -7,3 +7,13 @@ def index(request):
     return render(request,"book_outlet\index.html",{
         "books":book
     })
+
+def book_detail(request,id):
+    book = Book.objects.get(pk=id)
+    return render(request,"book_outlet\ook_detail.html",{
+        "title":book.title,
+        "author":book.author,
+        "rating":book.rating,
+        "is_bestselling":book.is_bestselling
+    })
+
