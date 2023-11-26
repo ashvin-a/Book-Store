ea8428f872c05d35316cc9b3a95f0ca058364b80
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2023-11-26 16:12:31 +0530

    updated all shit

--- book-store/book_outlet/models.py
+++ book-store/book_outlet/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Book (models.Model):
 
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    author = models.CharField(null=True,max_length=100)
+    is_bestselling = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.title},{self.rating}"
+        return f"{self.title},{self.rating},{self.author},{self.is_bestselling}"
--- book-store/book_outlet/views.py
+++ book-store/book_outlet/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+
+def index(request):
+    book = Book.objects.all()
+    return render(request,"book_outlet\index.html",{
+        "books":book
+    })
