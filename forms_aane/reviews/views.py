0238c5cb95150ba6531af035545121272e7d4fd9
Ashvinrockz <88977094+ashvin-a@users.noreply.github.com>
2023-11-27 22:21:18 +0530

    found out one messed up error

--- forms_aane/reviews/views.py
+++ forms_aane/reviews/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 def reviews(request):
-    return render(request,'reviews/reviews.html')
+    if request.method == 'POST':
+        data = request.POST["username"]
+        print(data)
+        return HttpResponseRedirect("/thankuu")
+    return render(request,"reviews/reviews.html")
+
+def thanku(request):
+    return render(request,"reviews/thank_you.html")
