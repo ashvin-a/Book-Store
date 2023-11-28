81e788dc31c80ef4540674bf6b234aef23bad65a
Ashvin <ashvin.anilkumar@qburst.com>
2023-11-28 09:25:56 +0530

    env stuff

--- forms_aane/reviews/views.py
+++ forms_aane/reviews/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import ReviewForm
 # Create your views here.
 def reviews(request):
     if request.method == 'POST':
-        data = request.POST["username"]
-        print(data)
-        return HttpResponseRedirect("/thankuu")
-    return render(request,"reviews/reviews.html")
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data        
+            print(data)
+            return HttpResponseRedirect("/thankuu")
+    
+    form = ReviewForm()
+    
+    return render(request,"reviews/reviews.html",{
+        "form":form
+    })
 
 def thanku(request):
     return render(request,"reviews/thank_you.html")
